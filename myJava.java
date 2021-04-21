50859be537cc1b51928e7b13730284c1b0e1d060
Çağatay ALSANCAK <24699441+cagatayalsancak@users.noreply.github.com>
2021-04-22 02:58:57 +0300

    myJava.java

---
+++ myJava.java
@@ -0,0 +1,10 @@
+public class main {
+
+	public static void main(String[] args) {
+		
+		//camelCase
+		//Don't repeat yourself. Kendini tekrar etme
+		String internetSubeButonu = "İnternet Şubeye gir";
+    
+  }
+}
